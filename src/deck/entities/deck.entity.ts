57f27df4f625e12409ee75b8687db8636c722a81
Carlos Henrique <carlossouzap@outlook.com>
2024-07-11 23:18:06 -0300

    feat: first changes to new strategy

--- src/deck/entities/deck.entity.ts
+++ src/deck/entities/deck.entity.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, ObjectType, createUnionType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 @ObjectType()
 @Schema()
@@ -11,25 +11,21 @@ export class Deck {
 
   @Field()
   @Prop({ required: true })
-  theme!: string;
+  title!: string;
 
-  //enum field
   @Field()
   @Prop({ required: true })
-  difficulty!: string;
+  theme!: string;
 
   @Field(() => [Card])
   @Prop({ required: true })
   cards!: Card[];
-
-  @Field()
-  @Prop({ required: true })
-  owner!: string;
 }
 
 @ObjectType()
 export class Card {
   @Field()
+  @Prop({ required: true })
   id!: string;
 
   @Field()
@@ -47,6 +43,66 @@ export class Card {
   @Field()
   @Prop({ required: true })
   category!: string;
+
+  @Field()
+  @Prop({ required: true })
+  difficulty!: string; // Dificuldade agora específica para cada card.
+}
+
+@ObjectType()
+@Schema()
+export class UserDeckResponse {
+  @Field(() => String)
+  @Prop({ required: true, ref: 'User' })
+  userId!: mongoose.Schema.Types.ObjectId; // Referência ao modelo de Usuário
+
+  @Field(() => String)
+  @Prop({ required: true, ref: 'Deck' })
+  deckId!: mongoose.Schema.Types.ObjectId; // Referência ao modelo de Deck
+
+  @Field(() => [String])
+  @Prop({ required: true })
+  selectedCardsIds!: mongoose.Schema.Types.ObjectId[]; // IDs dos cards selecionados
+
+  @Field()
+  @Prop({ required: true })
+  score!: number; // Pontuação do quiz
+
+  @Field(() => Date)
+  @Prop({ default: Date.now })
+  date!: Date; // Data da resposta do quiz
+
+  @Field(() => [UserCardMetrics])
+  @Prop({ required: true })
+  cardMetrics!: UserCardMetrics[]; // Métricas associadas a cada card
+}
+
+@ObjectType()
+@Schema()
+export class UserCardMetrics {
+  @Field(() => String)
+  @Prop({ required: true, ref: 'Card' })
+  cardId!: mongoose.Schema.Types.ObjectId; // Referência ao modelo de Card
+
+  @Field()
+  @Prop({ required: true, default: 0 })
+  repetitions!: number; // Número de vezes que o card foi revisado
+
+  @Field()
+  @Prop({ required: true, default: 2.5 })
+  easiness!: number; // Fator de facilidade no algoritmo de repetição
+
+  @Field()
+  @Prop({ required: true, default: 1 })
+  interval!: number; // Intervalo para a próxima revisão
+
+  @Field(() => Date)
+  @Prop({ required: true })
+  nextReviewDate!: Date; // Data da próxima revisão
+
+  @Field(() => Date)
+  @Prop({ default: Date.now })
+  lastReviewedDate!: Date; // Data da última revisão
 }
 
 @InputType()
